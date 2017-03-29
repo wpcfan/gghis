62ff18407779667759d1b7102f9c8a05c7893d07
Peng Wang <Peng Wang>
2017-03-29 19:43:19 +0800

    add rx logic to the form

--- src/app/domain/entities.interface.ts
+++ src/app/domain/entities.interface.ts
@@ -1,11 +1,52 @@
 import { RouterState } from '@ngrx/router-store';
 
+export enum IdentityType {
+  IdCard = 0,
+  Passport,
+  DriverLicense,
+  ResidenceBooklet,
+  SpecialDistrict,
+  Military,
+  Soldier,
+  Civilian,
+  Other
+}
+
+export enum BloodType {
+  UNKNOWN = 0,
+  A,
+  B,
+  AB,
+  O
+}
+
+export enum AgeUnit {
+  Year = 0,
+  Month,
+  Day
+}
+
 export interface User {
   id?: string;
   name: string;
   password?: string;
   gender?: boolean;
   phone?: string;
+}
+
+export interface Patient {
+  id: string;
+  name: string;
+  gender: boolean;
+  dateOfBirth: Date;
+  insuraceNo: string;
+  identityNo: string;
+  identityType: IdentityType;
+  bloodType: BloodType;
+  height: number;
+  weight: number;
+  phone: string;
+  addr: string;
 }
 
 export interface Err {
--- src/app/patient/patient-basic/patient-basic.component.ts
+++ src/app/patient/patient-basic/patient-basic.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators
+} from '@angular/forms';
+import { Patient, BloodType, AgeUnit, AppState } from '../../domain/entities.interface';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/combineLatest';
+import 'rxjs/add/operator/distinctUntilChanged';
+import * as moment from 'moment/moment';
 
 @Component({
   selector: 'app-patient-basic',
@@ -6,10 +17,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patient-basic.component.scss']
 })
 export class PatientBasicComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private store$: Store<AppState>) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      gender: ['', Validators.required],
+      bloodType: ['', Validators.required],
+      age: ['', Validators.required],
+      ageUnit: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      paymentMethod: ['', Validators.required],
+      phone: ['', Validators.required],
+      addr: ['']
+    });
+    const age$ = this.form.controls['age'].valueChanges.distinctUntilChanged();
+    const ageUnit$ = this.form.controls['ageUnit'].valueChanges.distinctUntilChanged();
+    const dateOfBirth$ = this.form.controls['dateOfBirth'].valueChanges.distinctUntilChanged();
+    const ageComputed$ = Observable.combineLatest(age$, ageUnit$, (a, u)=>{
+      if(a === undefined) return a;
+      switch (parseInt(u)) {
+        case AgeUnit.Day.valueOf():
+          return a;
+        case AgeUnit.Month.valueOf():
+          return a * 30;
+        case AgeUnit.Year.valueOf():
+        default:
+          return a * 365;
+      }
+    })
+    .filter(a=>a!=undefined)
+    .subscribe(x=>{
+      const date = moment().subtract(x, 'days').format('YYYY-MM-DD');
+      this.form.controls['dateOfBirth'].setValue(date);
+    });
+    
+  }
+
+  onSubmit() {
+    if(!this.form.valid) return;
+    const patient = {
+      name: this.form.controls['name'].value,
+      gender: this.form.controls['gender'].value,
+      bloodType: this.form.controls['bloodType'].value,
+      dateOfBirth: this.form.controls['dateOfBirth'].value,
+    }  
   }
 
 }
